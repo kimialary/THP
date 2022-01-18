beb859095ad0cadc3754fc13e0aae36a3acd60cb
kimialary <97555683+kimialary@users.noreply.github.com>
2022-01-18 21:16:34 +0100

    Exos ruby fun

---
+++ exos ruby/exo10.rb
@@ -0,0 +1,6 @@
+puts "Quel est ton année de naissance ?"
+print ">"
+year = 2017
+birth = gets.chomp.to_i
+
+puts "ton âge en 2017 :#{year - birth}"
---
+++ exos ruby/exo11.rb
@@ -0,0 +1,10 @@
+puts "Choisi un nombre"
+print ">"
+
+number = gets.chomp.to_i
+
+number.times do "Salut ça farte ?"
+	puts "Salut ça farte ?"
+
+end
+
